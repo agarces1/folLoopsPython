f48a961f5c96dd7281f2c386d7ed2b2d247c0433
Adrian Arroyo <adriangarces1@gmail.com>
2018-01-08 17:39:29 -0600

    forLoopsinpython3

---
+++ forLoopspython3/forLoopspython3/forLoopspython3.py
@@ -0,0 +1,5 @@
+#the idea for the while loop is to iterate through something, most of the times the while loop is used for finite tasks that have predetermined lenght, while and for loops are interchangable.
+
+exampleList = [1,5,6,6,2,1,5,2,1,4]
+    for numbers in exampleList:
+    print(numbers);
